438be18456fef1a00ec5f79eaeed6b772a6d21a9
Elena Ladutska <meduskalena@gmail.com>
2023-04-19 23:06:23 +0300

    changed logic and design

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
 const input = document.getElementById('text-input');
 const groceryList = document.getElementById('grocery-list');
-const eraser = document.getElementById('eraser');
+const btnDeleteAll = document.getElementById('deleteAll');
+const btnDeleteChosenItems = document.getElementById('deleteChosen');
+
+const addGood = () => {
+  const goodLi = document.createElement('li');
+  const goodA = document.createElement('a');
+  const goodText = document.createElement('p')
+
+  goodA.href = "#";
+  goodText.textContent = input.value;
+
+  goodLi.append(goodA);
+  goodA.append(goodText)
+  groceryList.append(goodLi);
+
+  goodLi.addEventListener('click', () => {
+    goodLi.classList.toggle('chosen');
+    checkTheList(btnDeleteChosenItems)
+  });
+  // ПЕРЕПИСАТЬ
+};
+
+const checkTheList = (btnName) => {
+  btnName.classList.add('active')
+};
 
 input.addEventListener('keydown', event => {
-  if( event.code === 'NumpadEnter' || event.code === 'Enter') {
+  if (( event.code === 'NumpadEnter' || event.code === 'Enter') && input.value) {
     addGood();
+    checkTheList(btnDeleteAll);
     input.value = '';
   };
 });
 
-const addGood = () => {
-  const good = document.createElement('li');
-  good.className ='good-li';
-  good.textContent = input.value;
-  groceryList.append(good);
-
-  good.addEventListener('click', function() {
-    good.classList.toggle('through');
-  });
-};
-
-eraser.addEventListener('click', function() {
+btnDeleteAll.addEventListener('click', () => {
   while (groceryList.firstChild) {
     groceryList.removeChild(groceryList.firstChild)
   };
+
+  btnDeleteAll.classList.toggle('active');
 });
+
+btnDeleteChosenItems.addEventListener('click', () => {
+  const items = Array.from(groceryList.children).filter(item => {
+    if (item.className === 'chosen') {
+      item.remove()
+    }
+  });
+
+  if (!items.length || !groceryList.children.length) {
+    btnDeleteChosenItems.classList.remove('active');
+  };
+
+  if (!groceryList.children.length) {
+    btnDeleteAll.classList.remove('active')
+  }
+})
+
